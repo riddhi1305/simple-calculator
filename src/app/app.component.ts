7bda001057e4c08d3ac907630da2b2cec8341f3b
riddhi1305 <101164001+riddhi1305@users.noreply.github.com>
2022-06-26 13:16:18 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mycalculator';
+  public num1!: number;
+  public num2!: number;
+  public num3!: number;
+
+  add(){
+    this.num3=this.num1+this.num2;
+  }
+  sub(){
+    this.num3=this.num1-this.num2;
+  }
+  multiply(){
+    this.num3=this.num1*this.num2;
+  }
+  divide(){
+    this.num3=this.num1/this.num2;
+  }
+}
